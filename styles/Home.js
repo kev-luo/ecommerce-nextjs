5db837e56c7bfb438db159452ab9f77cf1f81c3b
Kevin Luo <kvn.luo@gmail.com>
2020-12-14 22:20:01 -0500

    style home page product map

---
+++ styles/Home.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Product = styled.div`
+  padding: 24px;
+  font-size: 24px;
+`
+
+export const ProductRow = styled.div`
+  display: flex;
+`
+
+export const ProductColImage = styled.div`
+  flex: 1;
+  max-width: 50px;
+  margin-right: 25px;
+`
+
+export const ProductCol = styled.div`
+  flex: 1;
+`
