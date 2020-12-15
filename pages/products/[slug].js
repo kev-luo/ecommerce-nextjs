56ddebb641be14b1c840ed59a54998c0c2aad17f
Kevin Luo <kvn.luo@gmail.com>
2020-12-14 22:39:46 -0500

    hardcoded head tag content for product page

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import products from "../../products.json";
 import { fromImageToUrl } from "../../utils/urls";
 
@@ -5,12 +7,22 @@ const product = products[0];
 
 export default function Product() {
   return (
-    <div>
-      <h3>{product.name}</h3>
-      <img src={fromImageToUrl(product.image)} />
-      <h3>{product.name}</h3>
-      <p>${product.price}</p>
-      <p>{product.content}</p>
-    </div>
-  )
+    <>
+      <Head>
+        {product.meta_title && (
+          <title>{product.meta_title}</title>
+        )}
+        {product.meta_description && (
+          <meta name="description" content={product.meta_description} />
+        )}
+      </Head>
+      <div>
+        <h3>{product.name}</h3>
+        <img src={fromImageToUrl(product.image)} />
+        <h3>{product.name}</h3>
+        <p>${product.price}</p>
+        <p>{product.content}</p>
+      </div>
+    </>
+  );
 }
